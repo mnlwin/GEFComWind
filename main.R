286717a7aeb03bf596642452aee5fa7dc6636f16
mnlwin <m.lwin@utexas.edu>
2015-06-18 17:59:10 -0500

    minor

--- main.R
+++ main.R
@@ -6,14 +6,13 @@ rm(list=ls())
 cat("\014")
 
 #  Set any global variables here  ---------------------------------------------
-task = 14                    # week for raw data
-trainingWeek = 13            # week for training 
+task = 15                    # week for raw data
+trainingWeek = 14            # week for training 
 prevTrain.length = 15336     # number of rows in the training data for last week
 
 #  Run the code  --------------------------------------------------------------
 source("code/func.R")               #  load all functions
 source("code/load.R")               #  load raw data and required libraries
-#source("code/loadPrev.R")
 source("code/clean.R")              #  clean raw data and generate feature matrix
 #source("code/doCV.R")
 #source("code/doPrevScore.R")
